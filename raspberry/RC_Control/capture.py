8136a6e91d7da43b558cdd71962fc6c24a89e734
taeyonggwon <taaii6569@naver.com>
2018-05-09 19:22:55 +0900

    solve merge problem

--- raspberry/RC_Control/capture.py
+++ raspberry/RC_Control/capture.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import picamera
 import threading
 import time
@@ -34,41 +33,4 @@ class Capture(threading.Thread):
             os.mkdir("./" + path + "/")
     
     def __del__(self):
-=======
-import picamera
-import threading
-import time
-import os
-from datetime import datetime
-
-class Capture(threading.Thread):
-
-    def __init__(self, path, time, rc_car ):
-        super(Capture, self).__init__()
-        print("capture start")
-        #make camera object
-        self.camera = picamera.PiCamera()
-        #setting camera
-        self.camera.rotation = 180 #rotate 180
-        self.camera.resolution = (320, 240)#make picture size to 240x320
-        self.pause_time = time #interval time
-        self.is_dir(path)
-        self.path = path # path the data be stored
-        self.flag = True #thread flag
-        self.rc_car = rc_car
-
-    def run(self):
-        while self.flag:
-            get_filename = datetime.today().strftime("date_%H:%M:%S_streering_")+str(self.rc_car.angle)+"_speed_"+str(self.rc_car.speed)+".jpg"
-            filepath = os.path.join(self.path, get_filename)
-            #print("speed : ", rc_car.speed, "direct : ", rc_car.angle)
-            self.camera.capture(filepath)
-            time.sleep(self.pause_time)
-    
-    def is_dir(self, path):
-        if not os.path.isdir("./" + path + "/"):
-            os.mkdir("./" + path + "/")
-    
-    def __del__(self):
->>>>>>> 9bf5a7977fb5d85d3d0cf55790e78db2b33f8128
         print("capture end")
--- raspberry/RC_Control/panel.py
+++ raspberry/RC_Control/panel.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import threading
 import time
 
@@ -17,24 +16,4 @@ class Panel_Thread(threading.Thread, ):
             time.sleep(1)
 
     def __del__(self):
-=======
-import threading
-import time
-
-#show RC_CAR's status 
-class Panel_Thread(threading.Thread, ):
-
-    def __init__(self, rc_car):
-        super(Panel_Thread, self).__init__()
-        self.rc_car = rc_car
-        self.flag = True
-        print("Panel_Thread start")
-
-    def run(self):
-        while self.flag:
-            print("speed : ", self.rc_car.speed, "direct : ", self.rc_car.angle)
-            time.sleep(1)
-
-    def __del__(self):
->>>>>>> 9bf5a7977fb5d85d3d0cf55790e78db2b33f8128
         print('Panel del')
